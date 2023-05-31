6247601729f9d8834e45b16099e0f582d16ecc5d
Jphn <joaopedroholandaneves@gmail.com>
2023-05-31 09:59:45 -0300

    feat(login): create form states and handle submission

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logoLogin from '../assets/logoLogin.svg';
 import '../routes/Login.css';
+import { apiFetch } from '../utils/axios';
 
 const Login = () => {
+  const [[email, setEmail], [password, setPassword]] = [
+    useState(''),
+    useState(''),
+  ];
+
+  async function handleSubmission() {
+    const response = await apiFetch.post('/user/auth', {
+      email,
+      password,
+    });
+
+    if (response.status === 200) {
+      /*
+        Para o caso de tudo dar certo, e o usuário tiver digitado corretamente a senha e o email
+        a API irá retornar um token de autenticação que deverá ser guardado aqui no front
+      */
+    }
+  }
+
   return (
     <div className="row">
       <div className="col colOne">
@@ -21,16 +41,19 @@ const Login = () => {
               name="email"
               placeholder="E-mail"
               className="email"
+              onInput={(e) => setEmail(e.target.value)}
             />
             <input
               type="password"
               name="password"
               placeholder="Senha"
               className="password"
+              onInput={(e) => setPassword(e.target.value)}
             />
             <button
               type="submit"
               className="submit"
+              onClick={handleSubmission}
             >
               Entrar
             </button>
