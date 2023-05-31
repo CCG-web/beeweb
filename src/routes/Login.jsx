e582bc9ca781132bc9100976d19fcb989b21ac09
Jphn <joaopedroholandaneves@gmail.com>
2023-05-31 09:54:32 -0300

    refactor(login): format code as the style defined earlier

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,28 +1,57 @@
-import React from 'react'
-import "../routes/Login.css"
-import  logoLogin from "../assets/logoLogin.svg"
+import React from 'react';
+import logoLogin from '../assets/logoLogin.svg';
+import '../routes/Login.css';
+
 const Login = () => {
   return (
     <div className="row">
       <div className="col colOne">
-        <img src={logoLogin} alt="Imagem de exemplo"  className='center-image'/>
+        <img
+          src={logoLogin}
+          alt="Imagem de exemplo"
+          className="center-image"
+        />
       </div>
       <div className="col colTwo">
-        <div className='Formcontainer'>
-        <h1 className='login'>login</h1>
-        <form>
-          <input type="email" name="email" placeholder="E-mail" className='email' />
-          <input type="password" name="password" placeholder="Senha" className='password' />
-          <button type="submit" className='submit'>Entrar</button>
-        </form>
-        <a className='forgot' href="/">Esqueceu a senha?</a>
-        <label >Não tem conta?</label>
-        <a className='cadastre' href="/">Cadastre-se?</a>
+        <div className="Formcontainer">
+          <h1 className="login">login</h1>
+          <form>
+            <input
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              className="email"
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Senha"
+              className="password"
+            />
+            <button
+              type="submit"
+              className="submit"
+            >
+              Entrar
+            </button>
+          </form>
+          <a
+            className="forgot"
+            href="/"
+          >
+            Esqueceu a senha?
+          </a>
+          <label>Não tem conta?</label>
+          <a
+            className="cadastre"
+            href="/"
+          >
+            Cadastre-se?
+          </a>
+        </div>
       </div>
     </div>
-    </div>
-    
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
